3a4a27550bbf6a77db72092153fb86b5025d7498
phamanhquyet <q24052002@gmail.com>
2023-12-12 17:12:21 +0700

    responsive for sidebar

--- src/components/Dashboard/header/Dashboardheader.jsx
+++ src/components/Dashboard/header/Dashboardheader.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Dashboardheader.css";
 
 const Dashboardheader = () => {
-  return <div className="header-container">header</div>;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Clear local storage
+    localStorage.clear();
+
+    // Navigate to the home page
+    navigate("/");
+  };
+  return (
+    <div className="header-container">
+      <div></div>
+      <div>LOGO</div>
+      <button className="logout-button" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Dashboardheader;
