8c0f5f59c10a04fbd99abbaaaacb3efbc0678748
phamanhquyet <q24052002@gmail.com>
2023-12-19 16:38:56 +0700

    fix banner not working

--- src/components/Dashboard/Banner/Banner.jsx
+++ src/components/Dashboard/Banner/Banner.jsx
@@ -20,8 +20,8 @@ const Banner = () => {
     dots: false,
     beforeChange: (oldIndex, newIndex) => {
       if (newIndex >= 0 && newIndex < data?.data.length) {
-        const campaginId = data.data[newIndex].campaginId;
-        mutate({ id: campaginId });
+        const campaignId = data.data[newIndex].campaignId;
+        mutate({ id: campaignId });
       }
     },
     afterChange: (currentSlide) => {
@@ -38,12 +38,18 @@ const Banner = () => {
   if (isError) {
     return <p>Error fetching data</p>;
   }
+  if (data?.data.length === 0) {
+    refetch();
+    if (data?.data.length === 0) {
+      return <p>No banner to display</p>;
+    }
+  }
 
   return (
     <div className="dashboard_banner">
       <Carousel {...carouselSettings}>
         {data?.data.map((item) => (
-          <div key={item.campaginId} className="banner" style={{ ...contentStyle }}>
+          <div key={item.campaignId} className="banner" style={{ ...contentStyle }}>
             <img src={`${item.urlImg}`} alt="banner" />
           </div>
         ))}
